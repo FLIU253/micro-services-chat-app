8a0a4e6cabe2789c117392e92597b12f955460a9
Franklin Liu <FLIU253@uwo.ca>
2021-09-08 12:05:31 -0400

    styled login page

--- chat-app/src/index.tsx
+++ chat-app/src/index.tsx
@@ -7,11 +7,25 @@ import { RecoilRoot } from 'recoil';
 import { ApolloProvider } from '@apollo/client';
 import apolloClient from '#root/api/apolloClient';
 import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+	html, body {
+		margin: 0;
+		padding: 0;
+	}
+
+	html, body, #app {
+		height: 100%;
+		width: 100%;
+	}
+`;
 
 render(
 	<ApolloProvider client={apolloClient}>
 		<RecoilRoot>
 			<BrowserRouter>
+				<GlobalStyles />
 				<Root />
 			</BrowserRouter>
 		</RecoilRoot>
